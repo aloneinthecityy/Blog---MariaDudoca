11d60406596ddbed5c2e0c39c1896178ca0bd4b3
Maria Eduarda <duda.bastosmepb@gmail.com>
2023-11-24 16:16:42 -0300

    .

--- tina/config.ts
+++ tina/config.ts
@@ -2,7 +2,7 @@ import { CATEGORIES } from '../src/data/categories.ts'
 import { defineConfig } from 'tinacms'
 
 // Your hosting provider likely exposes this as an environment variable
-// const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || 'main'
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || 'main'
 
 export default defineConfig({
 	branch: 'main',
