57e5d443ccac9b780cb26fba4a2bc77bef42afbb
Maria Eduarda <duda.bastosmepb@gmail.com>
2023-11-24 19:39:51 -0300

    .

--- tina/config.ts
+++ tina/config.ts
@@ -7,7 +7,7 @@ const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || 'main'
 export default defineConfig({
 	branch: branch,
 	clientId: '933e4669-9e1d-4b80-be2e-d99b219b1bba', // Get this from tina.io
-	token: '164a25ea85872c5253e583d17681ddea5a8910ac', // Get this from tina.io
+	token: '1ebde6bfec94594ff9df22a4d0008419dce78e59', // Get this from tina.io
 
 	build: {
 		outputFolder: 'admin',
